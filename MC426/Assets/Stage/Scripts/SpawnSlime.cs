96d3ac5591d60076b989dcf9f61002fa196608d9
m4rquee <lucasxm12@gmail.com>
2020-04-18 13:28:23 -0300

    Slime spawner

---
+++ MC426/Assets/Stage/Scripts/SpawnSlime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSlime : MonoBehaviour {
+    
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private int delay = 10;
+
+    void Start() {
+        InvokeRepeating("Spawn", delay, delay); 
+    }
+
+    private void Spawn() {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+}
