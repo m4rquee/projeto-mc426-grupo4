721394a67f7b0d1bf80a28bd7975e315ea3ad9b0
m4rquee <lucasxm12@gmail.com>
2020-04-18 13:10:17 -0300

    Alcohol prefab

---
+++ MC426/Assets/Stage/Scripts/Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField]
+	private int total = 5;
+
+    public int damage(int amount) {
+		total -= amount; 
+		if (total <= 0)
+			Destroy(gameObject);
+		return total;
+    }
+}
