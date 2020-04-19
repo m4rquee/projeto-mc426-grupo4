06bc743f52c1a020d6d23f9384804beed2670615
daniel-campioni-playkids <daniel.campioni@playkids.com>
2020-04-19 16:13:54 -0300

    Added stage controller

---
+++ MC426/Assets/StageSelector/StageButtonSelector/Scripts/StageButtonPositions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageButtonPositions : ScriptableObject
+{
+    [SerializeField] private Transform[] _positions;
+
+    public Transform GetPosition(int index)
+    {
+        return _positions[index];
+    }
+
+    public bool HasPosition(int index)
+    {
+        return _positions[index] != null;
+    }
+}
---
+++ MC426/Assets/StageSelector/StageButtonSelector/Scripts/StageSelectorScreenController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Common.Broadcaster;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelectorScreenController : MonoBehaviour
+{
+    [SerializeField] private StageButtonPositions _positions;
+    [SerializeField] private GameObject _selectorButtonPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; _positions.HasPosition(i); i++)
+        {
+            var goButton = Instantiate(_selectorButtonPrefab, _positions.GetPosition(i));
+            var button = goButton.GetComponent<Button>();
+            button.onClick.AddListener(() => OpenPlayableScene(i));
+        }
+    }
+
+    void OpenPlayableScene(int sceneIndex)
+    {
+        // TODO: Open game scene
+        MessageBroadcaster.Instance.BroadcastMessage(new StageClickedMessage());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
