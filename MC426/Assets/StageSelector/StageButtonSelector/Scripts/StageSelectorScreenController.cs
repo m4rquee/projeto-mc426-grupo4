c53b06853011449eda358d66a481ec0a9283d5e0
d214777 <d214777@dac.unicamp.br>
2020-04-19 23:35:21 -0300

    Added warning when stage is unavailable

--- MC426/Assets/StageSelector/StageButtonSelector/Scripts/StageSelectorScreenController.cs
+++ MC426/Assets/StageSelector/StageButtonSelector/Scripts/StageSelectorScreenController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Common.Broadcaster;
 using UnityEngine;
@@ -9,10 +10,14 @@ public class StageSelectorScreenController : MonoBehaviour
 {
     [SerializeField] private StageButtonPositions _positions;
     [SerializeField] private GameObject _selectorButtonPrefab;
+    [SerializeField] private GameObject _blockedStageWarning;
+
+    private Coroutine _blockedStageCoroutine;
     
     // Start is called before the first frame update
     void Start()
     {
+        _blockedStageWarning.SetActive(false);
         for (var i = 0; _positions.HasPosition(i); i++)
         {
             var goButton = Instantiate(_selectorButtonPrefab, _positions.GetPosition(i));
@@ -26,9 +31,30 @@ public class StageSelectorScreenController : MonoBehaviour
 
     void OpenPlayableScene(int sceneIndex)
     {
-        // TODO: Open game scene
-        Debug.Log("Clicou no botão de entrar na fase número: " + (sceneIndex+1).ToString("00"));
-        MessageBroadcaster.Instance.BroadcastMessage(new StageClickedMessage(sceneIndex));
-        SceneManager.LoadScene("Stage");
+        //TODO: Verificar quando uma fase está bloqueada ou não para abrí-la
+        //por enquanto temos apenas a fase teste, então apenas verificamos a primeira
+        if (sceneIndex == 0)
+        {
+            _blockedStageWarning.SetActive(false);
+            Debug.Log("Clicou no botão de entrar na fase número: " + (sceneIndex + 1).ToString("00"));
+            MessageBroadcaster.Instance.BroadcastMessage(new StageClickedMessage(sceneIndex));
+            SceneManager.LoadScene("Stage");
+        }
+        else
+        {
+            _blockedStageWarning.SetActive(true);
+            _blockedStageCoroutine = StartCoroutine(CloseWarningAfterTime(5));
+        }
+    }
+
+    IEnumerator CloseWarningAfterTime(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        _blockedStageWarning.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        StopCoroutine(_blockedStageCoroutine);
     }
 }
