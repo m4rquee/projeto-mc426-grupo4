8a4d81d45e1808a57338c6a39c09c8d531e1e94a
munhozotavio <otaviosilmunhoz@gmail.com>
2020-05-16 17:11:04 -0300

    Created "sunflower" tower and fixed gel tower
    - Created tower "FICA EM CASA" that generates gold;
    - Fixed "ALCOOL GEL" tower to shoot and not generate gold
    - Created "BOLHA", the projectile sprite from "ALCOOL GEL" shoot

---
+++ MC426/Assets/Stage/Scripts/BulletDamage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Virus"){
+            col.GetComponent<Health>().damage(1);
+            Destroy (gameObject);
+        }
+    }
+}
---
+++ MC426/Assets/Stage/Scripts/TowerFire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFire : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public float interval;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Shoot",0,interval);
+    }
+
+    private void Shoot(){
+        // if (virusInFront()){}
+        if (true){
+            Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    private bool virusInFront(){
+        Vector2 origin = new Vector2(9.5f, transform.position.y);
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll(origin, -Vector2.right);
+
+        foreach(RaycastHit2D hit in hits) {
+            if(hit.collider != null && hit.collider.tag == "Zombie"){
+                return true;
+            }
+        }
+        return false;
+    }
+}
