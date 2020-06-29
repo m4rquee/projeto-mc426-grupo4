4e9da69bef8dbe4c5f12aea908f169bdc9069881
m4rquee <lucasxm12@gmail.com>
2020-06-28 22:06:47 -0300

    Unit test structure

---
+++ MC426/Assets/Tests/MenuTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MenuTest
+    {
+        public ShowStageBlockedWarningMessage ShowStageBlockedWarningMessage;
+        
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MenuTestSimplePasses()
+        {
+            Assert.True(true);
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator MenuTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            Assert.False(!true);
+            yield return null;
+        }
+    }
+}
