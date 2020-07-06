b06359cd2b37024fba81a2b06b3c85dd5b1c8391
m4rquee <lucasxm12@gmail.com>
2020-07-05 22:35:00 -0300

    Fixing export added

---
+++ MC426/Assets/Tests/DummyTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class DummyTest
+    {
+        [Test]
+        public void DummyTestSimplePasses()
+        {
+            Assert.True(!false);
+        }
+    }
+}    
